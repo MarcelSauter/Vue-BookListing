ff079d1836caa524ec8bdc9b19f24e84df145340
Marcel Sauter <msauter@proqrent.de>
2020-08-03 11:39:02 +0200

    4. module first try

---
+++ src/components/BookItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <li v-for="book in books">{{ book.title }}:{{ book.author }}</li>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'BookItem',
+    props: ['book']
+  }
+</script>
+
+<style>
+
+</style>
