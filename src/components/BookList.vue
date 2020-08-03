cc4a5ca23a9c7c7282ca86fef2f0472dbc8cbaea
Marcel Sauter <msauter@proqrent.de>
2020-08-03 11:21:28 +0200

    Second module first time finished

---
+++ src/components/BookList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      name: String,
+      title: String,
+      data() {
+        this.title = 'All Books';
+        return this.title;
+      }
+    },
+  }
+</script>
+
+<style>
+  h1, h2 {
+    font-weight: normal;
+  }
+</style>
